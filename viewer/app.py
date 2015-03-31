9a325e2ef97c98ce8b09b7698e6055eefb8ccd1f
Liam Andrew <liam.p.andrew@gmail.com>
2015-03-31 09:28:32 -0400

    Add link viewer application

---
+++ viewer/app.py
@@ -0,0 +1,69 @@
+from urlparse import urlparse
+
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from linker.querier import CustomStoryDatabase
+
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+DB_NAME = 'mclinksspider'
+
+def get_date_from_url(url):
+    try:
+        datelist = urlparse(url).path.split('/')[1:4]
+        for i, date in datelist:
+            if len(date) == 1:
+                datelist[i] = '0'+date
+        return '-'.join(datelist)
+    except:
+        return ''
+
+def get_title_from_url(url):
+    try:
+        title = urlparse(url).path.split('/')[-1]
+        if title == 'index.html':
+            title = urlparse(url).path.split('/')[-2]
+        return title
+    except:
+        return ''
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/hello.json')
+def hello_json():
+    url = request.args.get('url').strip()
+    mode = request.args.get('mode') or 'explore'
+    sort_mode = request.args.get('sortMode') or 'date'
+    c = CustomStoryDatabase(DB_NAME)
+
+    if mode == 'explore':
+        neighbors = c.getNeighbors(url)#, {'media_id': 104828})
+    elif mode == 'community':
+        neighbors = c.getSpiderCommunity(url, spider=3)# {'media_id': 104828}, spider=3)
+    elif mode == 'cocitation':
+        neighbors = c.getCocitations(url)#, {'media_id': 104828})
+
+    for i, neighbor in enumerate(neighbors):
+        neighbors[i].pop('_id', None)
+        if neighbors[i].get('href') is None:
+            neighbors[i]['href'] = neighbor.get('guid', neighbor.get('url'))
+        if neighbors[i].get('title') is None:
+            neighbors[i]['title'] = get_title_from_url(neighbor.get('href', neighbor.get('guid')))
+        if neighbors[i].get('publish_date') is None:
+            neighbors[i]['publish_date'] = get_date_from_url(neighbor.get('href', neighbor.get('guid')))
+
+    if sort_mode == 'date':
+        neighbors = sorted(neighbors, key=lambda n: n['publish_date'], reverse=True)
+    elif sort_mode == 'size':
+        neighbors = sorted(neighbors, key=lambda n: n.get('wordcount',0), reverse=True)
+    elif sort_mode == 'relevancy':
+        pass
+
+    return jsonify({'results': neighbors})
+
+if __name__ == '__main__' and __package__ is None:
+    app.run(debug=True)
---
+++ viewer/static/js/custom.js
@@ -0,0 +1,54 @@
+function populate_boxes(links) {
+	$('.list-group').each(function() {
+		if ($(this).find('.list-group-item').length === 0) {
+			var html = ""
+			$.each(links, function(key,value) {
+				var edge_class = value.is_in_edge == true ? 'inlink' : 'outlink';
+				var topic_class = value.href.substring(7,13) == 'topics' ? 'topic' : 'nontopic'
+				html += '<a href="'+value.href+'" class="list-group-item '+edge_class+' '+topic_class+'">';
+				html += '<h4 class="list-group-item-heading">'+value.title+'</h4>';
+				html += '<p class="list-group-item-text">'+value.publish_date+'</p>';
+				if (value.wordcount) {
+					html += '<p class="list-group-item-text"><em>Word count: '+value.wordcount+'</em></p>';
+				}
+				if (value.anchor) {
+					html += '<p class="list-group-item-text"><strong>'+value.anchor+'</strong> / &para; '+value.para+'</p>';
+				}
+				html += '</a>';
+			})
+			$(this).html(html);
+			return false;
+		}
+	})
+}
+
+function call_ajax_primary(event) {
+	var url = $(this).attr('href');
+	if (! url) {
+		var url = $('#url').val();
+		var nextCols = $('.col-sm-3')
+	}
+	else {
+		var nextCols = $(this).offsetParent().nextAll('.col-sm-3')
+	}
+	var nextCol = nextCols[0];
+	nextCols.find('ul').empty();
+	var spinner = new Spinner().spin(nextCol);
+	var radioMode = $('input[name=radio-opt]:checked').val();
+	var radioSortMode = $('input[name=radio-sort-opt]:checked').val();
+    $.ajax({
+          type: "GET",
+          url: "http://localhost:5000/hello.json?url="+url+"&mode="+radioMode+"&sortMode="+radioSortMode,
+          success: function(data) {
+          	spinner.stop();
+          	populate_boxes(data['results']);
+          },
+          dataType: 'json'
+    });
+    return false;
+}
+
+$(document).ready(function() {
+	$('.btn-primary').on('click', call_ajax_primary);
+	$('.list-group').on('click', '.list-group-item', call_ajax_primary);
+})
